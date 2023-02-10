d2b7a14170715130a18a3e197d347d57d9de9cb2
Paddy Mullen <paddy@paddymullen.com>
2023-02-10 16:33:33 -0500

    WIP on stylinig

--- src/components/DCFCell.tsx
+++ src/components/DCFCell.tsx
@@ -6,43 +6,20 @@ import { DFViewer } from "./DFViewer"
 import { requestDf } from "./utils"
 
 
-
-//@ts-ignore
-export function TransformedDf({instructions}) {
-
-   
-  const [schema, setSchema] = useState({})
-  const [columns, setColumns] = useState([])
-  const [rows, setRows]  = useState([])
-  useEffect(() => {
-  	       //fetch('http://localhost:8080/static-json/base-df.json')
-  	       fetch('http://localhost:5000/dcf/df/1?slice_start=3&slice_end=50')
-	       //fetch(`http://localhost:5000/dcf/transform_df/1?instructions=${transformInstructions(instructions)}&slice_start=3&slice_end=50`)
-	        .then(async (response) => {
-		   console.log(response)
-		   const tableDf = await response.json()
-		   setSchema(tableDf.schema)
-		   const [localColumns, localRows] = convertTableDF(tableDf)
-		   setColumns(localColumns)
-		   setRows(localRows)
-		   
-		   }
- );
- }, []);
-}
-
 //@ts-ignore
 export function DCFCell() {
   const [origDf, setOrigDf] = useState(tableDf)
   useEffect(() => {
-     	        requestDf('http://localhost:5000/dcf/df/1?slice_start=3&slice_end=8',
+     	        requestDf('http://localhost:5000/dcf/df/1?slice_start=3&slice_end=50',
 		setOrigDf)
  }, []);
         return (
-	    <div style={{width:'100%'}}>
+	    <div style={{width:'100%', height:"100%"}}>
+	        <h1 style={{height:"30px"}}>Data Cleaning Framework </h1>
+	        <div style={{height:"30%"}}>
 		<DFViewer df={origDf} />
+		</div>
 	        <ColumnsEditor df={origDf} />
-
 	    </div>
         );
 
--- src/components/DFViewer.tsx
+++ src/components/DFViewer.tsx
@@ -7,7 +7,7 @@ import { convertTableDF } from "./staticData";
 export function DFViewer({df}) {
   const [localColumns, localRows] = convertTableDF(df)
   return (
-    <div style={{width:'100%'}}>
+    <div style={{width:'100%',  "minHeight":"100px"}}>
       <DataGrid columns={localColumns} rows={localRows} />
     </div>
   );
