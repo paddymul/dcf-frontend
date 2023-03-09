5a7ee41d2fbcf5513e9940d644b5df858937b792
Paddy Mullen <paddy@paddymullen.com>
2023-03-09 17:08:10 -0500

    WIP

--- src/components/DoubleTableMenu.tsx
+++ src/components/DoubleTableMenu.tsx
@@ -15,79 +15,6 @@ function rowKeyGetter(row: any) {
   return row.index;
 }
 
-export function ContextCellMenu() {
-  const [contextMenuProps, setContextMenuProps] = useState<{
-    top: number;
-    left: number;
-  } | null>(null);
-  const menuRef = useRef<HTMLMenuElement | null>(null);
-  const isContextMenuOpen = contextMenuProps !== null
-  useLayoutEffect(() => {
-    if (!isContextMenuOpen) return;
-    function onClick(event: MouseEvent) {
-      if (event.target instanceof Node && menuRef.current?.contains(event.target)) {
-        return; }
-      setContextMenuProps(null); }
-    addEventListener('click', onClick);
-    return () => {
-      removeEventListener('click', onClick);};
-  }, [isContextMenuOpen]);
-
-  const row1:Record<string,any> = {}
-  columns.map((col:any) => {row1[col.key] = "false"})
-  const [columnSelectRows, setColumnSelect] = useState([row1])
-  return (
-    <div className="TableColumnsMenu" style={{padding:"0 10px 0 0"}}>
-      <DataGrid style={{height:"150px"}}
-        columns={columns}
-         rows = {columnSelectRows}
-        //@ts-ignore
-        onCellContextMenu={({ row, column }:any, event:any) => {
-	  console.log("column", column.name);
-
-	  const tempRow = _.clone(columnSelectRows[0])
-	  const oldVal = tempRow[column.key]
-	  tempRow[column.key] = oldVal == "false" ? "true": "false"
-	  setColumnSelect([tempRow])
-	  console.log("event", event);
-	  event.preventDefault();
-
-          setContextMenuProps({
-            top: event.clientY,
-            left: event.clientX
-          });
-	  console.log("after setContextMenuProps")
-        }}
-      />
-      {contextMenuProps !== null &&
-        createPortal(
-          <menu
-            ref={menuRef}
-            className="modal"
-            style={
-              {
-                top: contextMenuProps.top,
-                left: contextMenuProps.left
-              } as unknown as React.CSSProperties
-            }
-          >
-            <li><button
-                  onClick={() => {
-                    console.log("delete row clicked");
-                    setContextMenuProps(null);
-                  }}
-              >
-                Delete Row
-              </button>
-            </li>
-          </menu>,
-          document.body
-        )}
-    </div>
-  );
-
-}
-
 const sym = (symbolName:string) => {
   return {'symbol':symbolName}
 }
@@ -96,6 +23,63 @@ const bakedCommands = [
   [sym("dropcol"), sym('df'), 'col1'],
   [sym("fillna"), sym('df'), 'col2', 5]
 ]
+const UnknownCommand = [sym("nonexistent"), sym('df'), 'col1']
+
+
+const CommandPatterns:Record<string, any[]> = {
+  "dropcol":[],
+  "fillna":[[3, 'fillVal', 'type', 'integer']],
+  "resample":[[3, 'frequency', 'enum', ['daily', 'weekly', 'monthly']]],
+}
+
+//@ts-ignore
+const ArgGetter = ({argProps, val, setter}) => {
+
+  //@ts-ignore
+  const [argPos, label, argType, lastArg] = argProps
+
+  //type should actually be lined up with the column somehow, punting for now
+  if(argType === 'type') {
+    if(lastArg === 'integer'){
+      return (<fieldset>
+	<label> {label} </label>
+	  <input type="number" value={val} step="1" />
+	</fieldset>)
+    } else {
+      return (<fieldset>
+	<label> {label} </label>
+	  <input  value="dont know"/>
+	</fieldset>)
+    }
+  } else if (argType === 'enum') {
+    return (<fieldset>
+      <label> {label} </label>
+      <select>
+      //@ts-ignore
+      {lastArg.map((optionVal:any) => <option value={optionVal}>{optionVal}</option>)}
+	</select>
+      </fieldset>)
+  } else {
+    return <h3> unknown argtype </h3>
+  }
+
+}
+
+//@ts-ignore
+const CommandDetail = ({command}) => {
+  const commandName = command[0]['symbol']
+  const pattern = CommandPatterns[commandName]
+  if (! _.isArray(pattern)){
+    //we shouldn't get here
+    return <h2>unknown command {commandName}</h2>
+  } else if (pattern.length===0) {
+    return <h2>no arguments</h2>
+  } else {
+    
+  }
+  return <h2></h2>
+}
+
 
 
 //@ts-ignore
