d5946d07053b928905afca1de454bb0a2683cce8
Paddy Mullen <paddy@paddymullen.com>
2023-03-10 11:48:05 -0500

    detail from longer command list

--- src/components/Commands.tsx
+++ src/components/Commands.tsx
@@ -13,14 +13,66 @@ const CommandDetail = ({command}) => {
   if (! _.isArray(pattern)){
     //we shouldn't get here
     return <h2>unknown command {commandName}</h2>
-  } else if (pattern.length===0) {
+  } else if (_.isEqual(pattern, [null])) {
     return <h2>no arguments</h2>
   } else {
-    
+    //insert ArgGetter for each remaining argument
   }
   return <h2></h2>
 }
 
+
+const bakedCommands = [
+  [sym("dropcol"), sym('df'), 'col1'],
+  [sym("fillna"), sym('df'), 'col2', 5]
+]
+const UnknownCommand = [sym("nonexistent"), sym('df'), 'col1']
+
+const ArgNames =   ['Idx', 'label', 'specName',   'extraSpecArgs']
+type TypeSpec =    [number, string, 'type',       'integer' | 'float' | 'string']
+type EnumSpec =    [number, string, 'enum',       string[]]
+type ColEnumSpec = [number, string, 'colEnum',    string[]]
+type NoArgs = null
+type ArgSpec = TypeSpec | EnumSpec | ColEnumSpec | NoArgs
+
+const CommandPatterns:Record<string, ArgSpec[]> = {
+  "dropcol":[null],
+  "fillna":[[3, 'fillVal', 'type', 'integer']],
+  "resample":[[3, 'frequency', 'enum', ['daily', 'weekly', 'monthly']]],
+}
+
+//@ts-ignore
+const ArgGetter = ({argProps, val, setter}) => {
+  //@ts-ignore
+  const [argPos, label, argType, lastArg] = argProps
+  console.log("argProps", argProps)
+  //type should actually be lined up with the column somehow, punting for now
+  if (argType === 'enum') {
+    return (<fieldset>
+      <label> {label} </label>
+      <select>
+      //@ts-ignore
+      {lastArg.map((optionVal:any) => <option value={optionVal}>{optionVal}</option>)}
+	</select>
+      </fieldset>)
+  }
+  else if(argType === 'type') {
+    if(lastArg === 'integer'){
+      return (<fieldset>
+	<label> {label} </label>
+	  <input type="number" value={val} step="1" />
+	</fieldset>)
+    } else {
+      return (<fieldset>
+	<label> {label} </label>
+	  <input  value="dont know"/>
+	</fieldset>)
+    }
+  } else {
+    return <h3> unknown argtype </h3>
+  }
+
+}
 //@ts-ignore
 const CommandViewer = ({commands}) => {
   //@ts-ignore
@@ -39,59 +91,43 @@ const CommandViewer = ({commands}) => {
     rowEl[key] = element[2]
     return rowEl
   })
-
   const rows = [_.merge({}, ...rowElements)]
-  return <DataGrid style={{height:"150px"}}
+
+  //@ts-ignore
+  const commandObjs = _.map(Array.from(commands.entries()), ([index, element]) => {
+    const name = element[0]['symbol']
+    const key =  name + index.toString()
+    const rowEl: Record<string, any> = {}
+    rowEl[key] = element
+    return rowEl
+  })
+
+  const commandDict = _.merge({}, ...commandObjs)
+  //@ts-ignore
+  const [activeCommand, setActiveCommand] = useState(null)
+
+
+
+  return (<div>
+    <DataGrid style={{height:"150px"}}
         //@ts-ignore
         columns={columns}
         rows={rows}
-      />
-}
+        onCellClick={({ row, column }:any, event:any) => {
+	  console.log("column", column.key);
+          // add to generated commands
+	  const tempRow = _.clone(rows[0])
+	  const oldVal = tempRow[column.key]
+	  tempRow[column.key] = oldVal == "false" ? "true": "false"
+          //@ts-ignore
+	  setActiveCommand(commandDict[column.key])
+      //setColumnSelect([tempRow])
+      
+        }}
+    />
 
-const bakedCommands = [
-  [sym("dropcol"), sym('df'), 'col1'],
-  [sym("fillna"), sym('df'), 'col2', 5]
-]
-const UnknownCommand = [sym("nonexistent"), sym('df'), 'col1']
-
-
-const CommandPatterns:Record<string, any[]> = {
-  "dropcol":[],
-  "fillna":[[3, 'fillVal', 'type', 'integer']],
-  "resample":[[3, 'frequency', 'enum', ['daily', 'weekly', 'monthly']]],
-}
-
-//@ts-ignore
-const ArgGetter = ({argProps, val, setter}) => {
-
-  //@ts-ignore
-  const [argPos, label, argType, lastArg] = argProps
-  console.log("argProps", argProps)
-  //type should actually be lined up with the column somehow, punting for now
-  if(argType === 'type') {
-    if(lastArg === 'integer'){
-      return (<fieldset>
-	<label> {label} </label>
-	  <input type="number" value={val} step="1" />
-	</fieldset>)
-    } else {
-      return (<fieldset>
-	<label> {label} </label>
-	  <input  value="dont know"/>
-	</fieldset>)
-    }
-  } else if (argType === 'enum') {
-    return (<fieldset>
-      <label> {label} </label>
-      <select>
-      //@ts-ignore
-      {lastArg.map((optionVal:any) => <option value={optionVal}>{optionVal}</option>)}
-	</select>
-      </fieldset>)
-  } else {
-    return <h3> unknown argtype </h3>
-  }
-
+    { activeCommand && <CommandDetail command={activeCommand}/> }
+	  </div>)
 }
 
 
