1da5a079b82aecdc5e04be06c4ca80f3382a8b92
Paddy Mullen <paddy@paddymullen.com>
2023-03-13 13:29:28 -0400

    removed a lot of dead code

--- src/components/ColumnsEditor.tsx
+++ src/components/ColumnsEditor.tsx
@@ -1,76 +1,9 @@
 import React, { Component, useState, useEffect } from "react";
 import { tableDf } from "./staticData";
-import { propsToCommands, requestDf } from "./utils";
+import { requestDf } from "./utils";
 import { DFViewer } from "./DFViewer"
 import _ from 'lodash';
 import { CommandViewer } from './Commands'
-import { bakedCommands } from './CommandUtils'
-
-//import ContextMenuDemo from "./TableColumnMenu"
-import DoubleTableMenu from "./DoubleTableMenu"
-
-interface ColumnState {
-    drop:boolean
-    fillna:boolean
-}
-
-
-
-
-export function ColumnEditor(props:any) {
- const colState:any = props.colState
- const colStateChanged:any = props.colStateChanged
-
-   const handleDropChanged = () => {
-     colStateChanged({...colState, drop:!colState.drop}) }  
-   const fillNaChanged = (e:any) => {
-     colStateChanged({...colState, fillNa:!colState.fillNa}) }  
-   const fillNaValChanged = (e:any) => {
-     colStateChanged({...colState, fillNaVal:e.target.value}) }  
-
-   return (
-     	   <div className="column-editor">
-	       <label>
-		    <span> FillNa</span>
-		    <input
-		       type="checkbox"
-                       checked={colState.fillNa}
-		       onChange={fillNaChanged}/>
-                   <input
-                       value={colState.fillNaVal}
-		       onChange={fillNaValChanged}/>
-	       </label>
-	       <label>
-                  <span> Drop</span>
-                   <input
-		       type="checkbox"
-                       checked={colState.drop}
-		       onChange={handleDropChanged}/>
-	       </label>
-     </div>
-  )
-}
-
-
-//@ts-ignore
-function ColumnList({ fullProps, deepSet }) {
-  const [columnProps, setColumnProps] = useState({drop:false, fillNa:false, fillNaVal:"zsdf" })	
-
-  const listItems = _.keys(fullProps).map((name:any) =>
-    <div className="list-item" key={name} >
-        <dt>{name}</dt>
-        <dd>{fullProps[name].type}</dd>
-        <ColumnEditor key={name} colState={fullProps[name]} colStateChanged={deepSet([name])}/>
-    </div>);
-
-  return (<div className="column-list" >
-              <dl>{listItems}</dl>
-	  </div>)
-}
-
-
-
-
 
 //@ts-ignore
 export function CommandDisplayer({filledCommands, style}) {
@@ -91,25 +24,23 @@ export function PythonDisplayer({filledCommands, style}) {
   const emptyUrl = `${URLBase}df/1?${sliceArgs}`
 
   useEffect(() => {
-     
-
-  const pyCodeUrl = `${URLBase}dcf_to_py/1?instructions=${JSON.stringify(filledCommands)}`
-  if(filledCommands.length == 0) {
-     return
-  }
-  else {
-    fetch(pyCodeUrl)
-      .then(async (response) => {
-	console.log(response)
-	//@ts-ignore	
-	const fullResp = await response.json()
-	console.log("fullResp", fullResp)
-	//@ts-ignore	
-	const pyCode = fullResp['py']
-	console.log("pyCode", pyCode)
-	setPyString(pyCode)
-      });
-  }
+    const pyCodeUrl = `${URLBase}dcf_to_py/1?instructions=${JSON.stringify(filledCommands)}`
+    if(filledCommands.length == 0) {
+      return
+    }
+    else {
+      fetch(pyCodeUrl)
+	.then(async (response) => {
+	  console.log(response)
+	  //@ts-ignore	
+	  const fullResp = await response.json()
+	  console.log("fullResp", fullResp)
+	  //@ts-ignore	
+	  const pyCode = fullResp['py']
+	  console.log("pyCode", pyCode)
+	  setPyString(pyCode)
+	});
+    }
   }, [filledCommands]);
   const baseStyle = {margin:"0",  textAlign:"left"}
   const localStyle = {...baseStyle, ...style } 
@@ -124,33 +55,28 @@ const transformInstructions = (raw) => {
       return JSON.stringify([{'symbol':'begin'}, ...raw])
 }
 
-
 //@ts-ignore
 export function TransformViewer({ filledCommands, style }) {
   const [transDf, setTransDf] = useState(tableDf)
-
   const URLBase = "http://localhost:5000/dcf/"
   const sliceArgs = "slice_start=3&slice_end=50"
   const emptyUrl = `${URLBase}df/1?${sliceArgs}`
 
   useEffect(() => {
      
-  const instructions = transformInstructions(filledCommands)
-  console.log("filledCommands", filledCommands)
-  console.log("instructions", instructions)
-
-  const transUrl = `${URLBase}transform_df/1?instructions=${instructions}&${sliceArgs}`
-  if(filledCommands.length == 0) {
-    requestDf(emptyUrl, setTransDf);
-  }
-  else {
-   requestDf(transUrl, setTransDf);
-  }
+    const instructions = transformInstructions(filledCommands)
+    console.log("filledCommands", filledCommands)
+    console.log("instructions", instructions)
+    const transUrl = `${URLBase}transform_df/1?instructions=${instructions}&${sliceArgs}`
+    if(filledCommands.length == 0) {
+      requestDf(emptyUrl, setTransDf);
+    }
+    else {
+      requestDf(transUrl, setTransDf);
+    }
   }, [filledCommands]);
   return (<div className="transform-viewer"> <DFViewer style={style}  df={transDf} /> </div>);
 }
-
-
 
 //@ts-ignore
 export function DependentTabs({ filledCommands }) {
@@ -190,41 +116,19 @@ export function DependentTabs({ filledCommands }) {
 	</div>)
 }
 
-
-
 //@ts-ignore
-export function ColumnsEditor({ df }) {
-
+export function ColumnsEditor(
+  { df, activeColumn }:{df:any, activeColumn?:string} =
+    {df:{}, activeColumn:'stoptime'}) {
   const schema = df.schema;
-  const baseState = {drop:false, fillNa:false, fillNaVal:"zsdf", type:'foo' }
-  const totalProps:Record<string, any> = {}
-  schema.fields.map((f:any) => {
-        const props = _.clone(baseState)
-	props.type = f.type
-  	totalProps[f.name] = props
-  })
-  console.log("totalProps", totalProps)
-
-  const [fullProps, setFullProps] = useState(totalProps)
-  const deepSetColumnProps = (field:string) => {
-      const retFunc = (newSingleColumnProps:any) => {
-            const baseProps = _.cloneDeep(fullProps)
-	    baseProps[field] = newSingleColumnProps
-	    setFullProps(baseProps)
-      }
-      return retFunc
-  }
-
-  const [commands, setCommands] = useState(bakedCommands)
+  const [commands, setCommands] = useState([])
   const allColumns = df.schema.fields.map((field:any) => field.name)
   return (<div className="columns-editor" style={{width:'100%',    }}>
     <CommandViewer commands={commands} setCommands={setCommands} 
-	  activeColumn={'stoptime'}
+	  activeColumn={activeColumn}
 	  allColumns={allColumns} />
 
-	      <DependentTabs filledCommands={commands}/>
-	</div>)
+    <DependentTabs filledCommands={commands}/>
+    </div>)
 }
-
-
 
